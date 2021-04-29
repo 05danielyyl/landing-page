1fdf4f0be224d1b65f4ce3846874d25729cb6eaa
05danielyyl <71957170+05danielyyl@users.noreply.github.com>
2021-04-29 15:59:09 -0400

    Add files via upload

---
+++ split_landing/index.js
@@ -0,0 +1,13 @@
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+left.addEventListener("mouseenter", function(){
+    container.classList.remove('hover-right');
+    container.classList.add('hover-left');
+})
+
+right.addEventListener("mouseenter", function(){
+    container.classList.remove('hover-left');
+    container.classList.add('hover-right');
+})
